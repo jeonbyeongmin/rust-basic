856e475ec1060926e421161a07146c72f2385eb4
jeonbyeongmin <qudals7613@gmail.com>
2022-12-16 15:08:49 +0900

    feat: Rust IO Practice

---
+++ greprs/src/main.rs
@@ -0,0 +1,19 @@
+extern crate greprs;
+
+use greprs::Config;
+
+use std::env;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = greprs::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
